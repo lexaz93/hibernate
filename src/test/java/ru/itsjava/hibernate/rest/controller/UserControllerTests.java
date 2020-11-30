24f397004513cd193c2cd5f6c223776c33f87f78
lexaz93 <lexaz93@mail.ru>
2020-11-30 21:16:42 +0400

    Тесты на UserController

---
+++ src/test/java/ru/itsjava/hibernate/rest/controller/UserControllerTests.java
@@ -0,0 +1,52 @@
+package ru.itsjava.hibernate.rest.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.itsjava.hibernate.services.repository.UserRepository;
+import ru.itsjava.hibernate.services.rest.controller.UserController;
+import ru.itsjava.hibernate.services.services.UserService;
+import ru.itsjava.hibernate.services.services.UserServiceImpl;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("UserController have:")
+@WebMvcTest(UserController.class)
+public class UserControllerTests {
+    @Configuration
+    static class TestConfiguration {
+        @MockBean
+        private UserRepository userRepository;
+
+        @Bean
+        public UserService userService() {
+            return new UserServiceImpl(userRepository);
+        }
+
+        @Bean
+        public UserController userController() {
+            return new UserController(userService());
+        }
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserController userController;
+
+    @DisplayName("correct method getUserList().")
+    @Test
+    public void shouldCorrectMethodGetUserList() throws Exception {
+        this.mockMvc.perform(get("/")).andExpect(status().isOk());
+    }
+}

211790f9684524f6140c723867d19b51bd68bc06
lexaz93 <lexaz93@mail.ru>
2020-12-07 15:06:55 +0400

    Поменял вывод пользователя

--- src/main/java/ru/itsjava/project/HibernateApplication.java
+++ src/main/java/ru/itsjava/project/HibernateApplication.java
@@ -18,11 +18,19 @@ public class HibernateApplication {
 	public static void main(String[] args) throws SQLException {
 		var context = SpringApplication.run(HibernateApplication.class, args);
 		Mail mail = new Mail(0l, "alex@mail.ru");
+		Mail mail2 = new Mail(0l, "anna@mail.ru");
+		Mail mail3 = new Mail(0l, "friend@mail.ru");
 		Pet pet = new Pet(0l,"Cat", "Musya");
+		Pet pet2 = new Pet(0l,"Dog", "Husky");
+		Pet pet3 = new Pet(0l,"Catty", "Nyusha");
 		User user = new User(0l, "Alex", 27, mail, pet);
+		User user2 = new User(0l, "Anna", 26, mail2, pet2);
+		User user3 = new User(0l, "Andrey", 28, mail3, pet3);
 		UserService userService = context.getBean(UserServiceImpl.class);
 		userService.createUser(user);
-		Console.main(args);
+		userService.createUser(user2);
+		userService.createUser(user3);
+//		Console.main(args);
 	}
 
 }
--- src/main/java/ru/itsjava/project/rest/controller/UserDto/UserDto.java
+++ src/main/java/ru/itsjava/project/rest/controller/UserDto/UserDto.java
@@ -2,6 +2,8 @@ package ru.itsjava.project.rest.controller.UserDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.itsjava.project.model.Mail;
+import ru.itsjava.project.model.Pet;
 import ru.itsjava.project.model.User;
 
 @Data
@@ -10,12 +12,15 @@ public class UserDto {
     private long id;
     private String name;
     private int age;
+    private String mail;
+    private String typePet;
+    private String pet;
 
     public static User toDomainObject(UserDto userDto) {
-        return new User(userDto.id, userDto.name, userDto.age);
+        return new User(userDto.id, userDto.name, userDto.age, new Mail(userDto.id, userDto.mail), new Pet(userDto.id, userDto.typePet, userDto.pet));
     }
 
     public static UserDto toDto(User user) {
-        return new UserDto(user.getId(), user.getName(), user.getAge());
+        return new UserDto(user.getId(), user.getName(), user.getAge(), user.getEmail().getEmail(), user.getPet().getTypePet(), user.getPet().getPetName());
     }
 }
